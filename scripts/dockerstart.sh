6ce7a6d2bcc2c0af26acff03c725df9d36f14c6e
Aniketh A <aaatipamula@gmail.com>
2022-09-11 00:28:58 -0500

    added line to start bot

--- scripts/dockerstart.sh
+++ scripts/dockerstart.sh
@@ -9,6 +9,8 @@ then
         echo 'Starting containers...'
         sudo docker run -it --name bot -d bot
     
+        echo 'Starting bot...'
+        sudo docker exec -it -d bot python3 apcsp_bot.py
 else
     echo 'Please navigate to the home directory of this project'
     exit
